640783d8255a7fb21f05d1c89b34fcbb8f0fb8af
xiaohh <1296522554@qq.com>
2021-10-11 17:03:00 +0800

    封装Tabnav组件

---
+++ src/components/Tabnav.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="tabnav">
+    <div class="logo">
+      <router-link to="/">🍊 Orange-UI</router-link>
+    </div>
+    <div class="meun">
+      <div class="items1">
+        <router-link to="/">指南</router-link>
+      </div>
+      <div class="items2">
+        <router-link to="/">GitHub</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style scoped>
+.tabnav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+}
+.tabnav > .logo {
+  margin-left: 10px;
+}
+
+.tabnav > .meun {
+  display: flex;
+  align-items: center;
+}
+.tabnav > .meun > div {
+  font-size: 14px;
+  margin-right: 40px;
+}
+</style>
---
+++ src/views/Doc.vue
@@ -0,0 +1,13 @@
+<template>
+ <Tabnav/>
+  1212
+</template>
+<script lang="ts">
+import Tabnav from "../components/Tabnav.vue";
+export default {
+  components: {Tabnav}
+}
+</script>
+
+<style scoped>
+</style>
