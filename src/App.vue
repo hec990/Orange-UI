7d3bf7e5bbc3a3b0c2b4de6eba555fa1f8e9c534
xiaohh <1296522554@qq.com>
2021-10-11 11:55:46 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <XiaoHe />
+  </div>
+</template>
+
+<script>
+import XiaoHe from "./components/XiaoHe.vue";
+
+export default {
+  name: "App",
+  components: {
+    XiaoHe,
+  },
+};
+</script>
