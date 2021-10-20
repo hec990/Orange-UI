2cf523e7b00b1af535cd49fff2771e06181bcbd2
xiaohh <1296522554@qq.com>
2021-10-20 17:40:02 +0800

    完成首页home-Tabnav组件页面开发

---
+++ src/components/home-Tabnav.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="tabnav">
+   <div class="logo">
+     <img src="../../src/assets/images/gulu.png">
+   </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.tabnav {
+  background-color: #FD9C05;
+  height: 60px;
+}
+img {
+  width: 28px;
+  margin-top: 16px;
+  margin-left: 10px;
+  border: 1px solid red;
+}
+
+
+
+
+// 手机
+@media only screen and (max-device-width: 414px){
+  .logo > .logo-img > img {
+    opacity: 1;
+  }
+}
+// ipad
+@media only screen and (min-device-width: 768px){
+  .logo > a {
+    display: block;
+  }
+}
+</style>
